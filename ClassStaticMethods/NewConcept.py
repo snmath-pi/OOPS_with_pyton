196e433a24df4d6b1aed617530ce14e1bb2f9257
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-03-08 10:44:32 +0530

    c

--- ClassStaticMethods/NewConcept.py
+++ ClassStaticMethods/NewConcept.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     pay_rate = 0.8
     all = []
@@ -20,14 +22,20 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate #using Item gives for item but self changes it 
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item (
+                
+            )
+        
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
         
-item1 = Item('Phone', 100, 1)
-item2 = Item('Laptop', 1000, 3)
-item3 = Item('Cable', 10, 5)
-item4 = Item('Mouse', 50, 5)
-item5 = Item('Keyboard', 75, 5)
 
-print(Item.all)
+
+Item.instantiate_from_csv()
 
